bf3ea3fb53958ac2307048e687b9f25230eea066
imam-programmer <imamprogrammer09@gmail.com>
2026-07-23 23:27:44 +0600

    key set and data fetching done by key

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
+
   return (
-    <div className='h-screen bg-gray-950 text-amber-50'>App</div>
+    <div className='h-screen bg-gray-950 text-amber-50'>
+      <button onClick={async ()=>{   
+   const data=await fetchPhotos("dog")
+   console.log(data.results);  
+      }
+      }>Get photos</button>
+           <button className='bg-red-500' onClick={async ()=>{   
+   const data=await fetchVideos("cat")
+   console.log(data.videos);  
+      }
+      }>Get photos</button>
+    </div>
   )
 }
 
